fe13f0f0b5530e89a35cfd01f35d6ed75ab4dbe5
BeverageBev <89879144+BeverageBev@users.noreply.github.com>
2021-10-20 20:58:05 -0500

    Comepletely updated dumbass emilly code
    fuck off emilly

--- js/heroheadercycle.js
+++ js/heroheadercycle.js
@@ -1,11 +1,11 @@
 var text=[
-    "The box with more storage than an actual box.",
-    "The best thing since sliced bread. 🍞",
-    "The something something something.",
-    "The ultimate host for your grandma's photo collection.",];
+    "The most storage possible!",
+    "The best innovation since ovhs new hosting area!",
+    "Ultimate Solution for the Best Price!",
+    "The ultimate host for your team!",];
     
     var counter=0;
     var elem=document.getElementById("heroText");
-    var inst=setInterval(change,3500);
+    var inst=setInterval(change,6000);
     function change(){elem.innerHTML=text[counter];
         counter++;if(counter>=text.length){counter=0}}
